0f3e3a3542cd14e64143ab3f8acbfe29181a9930
DilshanGAM <it21180934@my.sliit.lk>
2025-03-06 16:59:38 +0530

    typo in init fixed 😘

--- app/utils/db.py
+++ app/utils/db.py
@@ -9,7 +9,7 @@ class Database:
         mongo_uri = os.getenv("MONGO_URI")
         db_name = os.getenv("MONGO_DB_NAME")
         
-        if not mongo_uri or not db_name:
+        if not mongo_uri or not db_name:             
             raise ValueError("MONGO_URI and MONGO_DB_NAME must be set in .env")
 
         self.client = MongoClient(mongo_uri)
